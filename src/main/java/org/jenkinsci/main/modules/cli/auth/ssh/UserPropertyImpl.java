2f25c3269c252090b09c60d5b38647f4bae16493
Oliver Gondža <ogondza@redhat.com>
2013-11-24 20:17:39 +0100

    Expose PublicKeySignatureWriter

--- src/main/java/org/jenkinsci/main/modules/cli/auth/ssh/UserPropertyImpl.java
+++ src/main/java/org/jenkinsci/main/modules/cli/auth/ssh/UserPropertyImpl.java
@@ -1,25 +1,22 @@
 package org.jenkinsci.main.modules.cli.auth.ssh;
 
-import com.trilead.ssh2.crypto.Base64;
-import com.trilead.ssh2.packets.TypesWriter;
 import hudson.Extension;
-import hudson.model.User;
 import hudson.model.UserProperty;
 import hudson.model.UserPropertyDescriptor;
-import org.kohsuke.stapler.DataBoundConstructor;
+import hudson.model.User;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.security.PublicKey;
-import java.security.interfaces.DSAParams;
-import java.security.interfaces.DSAPublicKey;
-import java.security.interfaces.RSAPublicKey;
+
+import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 public class UserPropertyImpl extends UserProperty {
+    private static final PublicKeySignatureWriter signature = new PublicKeySignatureWriter();
     public String authorizedKeys;
 
     @DataBoundConstructor
@@ -31,7 +28,7 @@ public class UserPropertyImpl extends UserProperty {
      * Checks if this user has the given public key in his {@link #authorizedKeys}.
      */
     public boolean has(PublicKey pk) {
-        return isAuthorizedKey(getPublicKeySignature(pk));
+        return isAuthorizedKey(signature.asString(pk));
     }
 
     public boolean isAuthorizedKey(String sig) {
@@ -61,34 +58,12 @@ public class UserPropertyImpl extends UserProperty {
     }
 
     public static User findUser(PublicKey identity) {
-        String sig = getPublicKeySignature(identity);
+        String sig = signature.asString(identity);
         for (User u : User.getAll()) {
             UserPropertyImpl p = u.getProperty(UserPropertyImpl.class);
             if (p!=null && p.isAuthorizedKey(sig))
                 return u;
         }
         return null;
-    }
-
-    private static String getPublicKeySignature(PublicKey pk) {
-        TypesWriter tw = new TypesWriter();
-        if (pk instanceof RSAPublicKey) {
-            RSAPublicKey rpk = (RSAPublicKey) pk;
-            tw.writeString("ssh-rsa");
-            tw.writeMPInt(rpk.getPublicExponent());
-            tw.writeMPInt(rpk.getModulus());
-            return new String(Base64.encode(tw.getBytes()));
-        }
-        if (pk instanceof DSAPublicKey) {
-            DSAPublicKey rpk = (DSAPublicKey) pk;
-            tw.writeString("ssh-dss");
-            DSAParams p = rpk.getParams();
-            tw.writeMPInt(p.getP());
-            tw.writeMPInt(p.getQ());
-            tw.writeMPInt(p.getG());
-            tw.writeMPInt(rpk.getY());
-            return new String(Base64.encode(tw.getBytes()));
-        }
-        throw new IllegalArgumentException("Unknown key type: "+pk);
     }
 }
