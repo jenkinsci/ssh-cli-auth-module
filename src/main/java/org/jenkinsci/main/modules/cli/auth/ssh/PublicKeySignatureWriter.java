f5a0556a16760cfa008828e56ffb12f03a4dc3a3
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-15 12:58:01 +0200

    fix: refactor methods to a Helper class

--- src/main/java/org/jenkinsci/main/modules/cli/auth/ssh/PublicKeySignatureWriter.java
+++ src/main/java/org/jenkinsci/main/modules/cli/auth/ssh/PublicKeySignatureWriter.java
@@ -25,9 +25,6 @@ package org.jenkinsci.main.modules.cli.auth.ssh;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.math.BigInteger;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.interfaces.DSAParams;
 import java.security.interfaces.DSAPublicKey;
@@ -44,66 +41,31 @@ public class PublicKeySignatureWriter {
         throw new IllegalArgumentException("Unknown key type: " + key);
     }
 
-    /*
-     * copied from https://github.com/apache/mina-sshd/blob/master/sshd-common/src/main/java/org/apache/sshd/common/config/keys/impl/DSSPublicKeyEntryDecoder.java
-     */
     public String asString(DSAPublicKey key) {
         try {
             ByteArrayOutputStream output = new ByteArrayOutputStream();
             DSAParams keyParams = Objects.requireNonNull(key.getParams(), "No DSA params available");
-            encodeString(output, "ssh-dss", StandardCharsets.UTF_8);
-            encodeBigInt(output, keyParams.getP());
-            encodeBigInt(output, keyParams.getQ());
-            encodeBigInt(output, keyParams.getG());
-            encodeBigInt(output, key.getY());
+            KeyEncodeHelper.encodeString(output, "ssh-dss", StandardCharsets.UTF_8);
+            KeyEncodeHelper.encodeBigInt(output, keyParams.getP());
+            KeyEncodeHelper.encodeBigInt(output, keyParams.getQ());
+            KeyEncodeHelper.encodeBigInt(output, keyParams.getG());
+            KeyEncodeHelper.encodeBigInt(output, key.getY());
             return new String(output.toByteArray(), StandardCharsets.UTF_8);
         } catch(IOException e) {
             throw new Error(e);
         }
     }
 
-    /*
-     * copied from https://github.com/apache/mina-sshd/blob/master/sshd-common/src/main/java/org/apache/sshd/common/config/keys/impl/RSAPublicKeyDecoder.java
-     */
     public String asString(RSAPublicKey key) {
         try {
             ByteArrayOutputStream output = new ByteArrayOutputStream();
-            encodeString(output, "ssh-rsa", StandardCharsets.UTF_8);
-            encodeBigInt(output, key.getPublicExponent());
-            encodeBigInt(output, key.getModulus());
+            KeyEncodeHelper.encodeString(output, "ssh-rsa", StandardCharsets.UTF_8);
+            KeyEncodeHelper.encodeBigInt(output, key.getPublicExponent());
+            KeyEncodeHelper.encodeBigInt(output, key.getModulus());
             return new String(output.toByteArray(), StandardCharsets.UTF_8);
         } catch(IOException e) {
             throw new Error(e);
         }
     }
 
-    /*
-     * copied from https://github.com/apache/mina-sshd/blob/master/sshd-common/src/main/java/org/apache/sshd/common/config/keys/KeyEntryResolver.java
-     */
-    private int encodeString(OutputStream s, String v, Charset cs) throws IOException {
-        byte[] bytes = v.getBytes(cs);
-        return writeRLEBytes(s, bytes, 0, bytes.length);
-    }
-
-    private int encodeBigInt(OutputStream s, BigInteger v) throws IOException {
-        byte[] bytes = v.toByteArray();
-        return writeRLEBytes(s, bytes, 0, bytes.length);
-    }
-
-    private int writeRLEBytes(OutputStream s, byte[] bytes, int off, int len) throws IOException {
-        byte[] lenBytes = encodeInt(s, len);
-        s.write(bytes, off, len);
-        return lenBytes.length + len;
-    }
-
-    private byte[] encodeInt(OutputStream s, int v) throws IOException {
-        byte[] bytes = {
-                (byte) ((v >> 24) & 0xFF),
-                (byte) ((v >> 16) & 0xFF),
-                (byte) ((v >> 8) & 0xFF),
-                (byte) (v & 0xFF)
-        };
-        s.write(bytes);
-        return bytes;
-    }
 }
